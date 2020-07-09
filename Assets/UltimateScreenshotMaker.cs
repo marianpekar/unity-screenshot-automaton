5ed767d46c18266c97e289be6b4bf661b3ad4e08
Marian Pekár <marian.pekar@gmail.com>
2020-07-09 20:49:52 +0200

    Change UltimateScreenshotMaker to handle null objects

--- Assets/UltimateScreenshotMaker.cs
+++ Assets/UltimateScreenshotMaker.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class UltimateScreenshotMaker : MonoBehaviour
@@ -14,6 +15,10 @@ public class UltimateScreenshotMaker : MonoBehaviour
 
     [SerializeField] private Light[] lights;
 
+    [SerializeField] private bool UseOnlyMainCamera;
+
+    [SerializeField] public bool LookAtTarget;
+
     [SerializeField] private ScreenshotCamera[] cameras;
 
     [SerializeField] private GameObject[] prefabs;
@@ -26,6 +31,20 @@ public class UltimateScreenshotMaker : MonoBehaviour
 
     void Start()
     {
+        if (!target)
+        {
+            target = new GameObject().transform;
+            target.name = "Target";
+        }
+
+        InitializePool();
+        DisableAllLights();
+        SetupCameras();
+        StartCoroutine(MainCoroutine());
+    }
+
+    private void InitializePool()
+    {
         pool = new GameObject[prefabs.Length];
 
         for (var i = 0; i < prefabs.Length; i++)
@@ -34,20 +53,54 @@ public class UltimateScreenshotMaker : MonoBehaviour
             pool[i] = instance;
             instance.SetActive(false);
         }
+    }
+
+    private void DisableAllLights()
+    {
+        if (lights.Length == 0)
+        {
+            lights = new Light[1];
+            var lightGameObject = new GameObject().AddComponent<Light>();
+            lightGameObject.name = "DummyLight";
+            lightGameObject.intensity = 0;
+            lightGameObject.range = 0;
+            lights[0] = lightGameObject;
+            return;
+        }
 
         foreach (var light in lights)
             light.enabled = false;
+    }
+
+    private void SetupCameras()
+    {
+        if (cameras.Length == 0 || UseOnlyMainCamera)
+        {
+            var mainCamera = Camera.main;
+
+            foreach (var camera in cameras)
+                camera.Camera.gameObject.SetActive(false);
+
+            mainCamera.gameObject.SetActive(true);
+
+            cameras = new ScreenshotCamera[]
+            {
+                new ScreenshotCamera
+                {
+                    Camera = mainCamera,
+                    LookAtTarget = LookAtTarget
+                }
+            };
+        }
 
         foreach (var camera in cameras)
         {
-            if(camera.LookAtTarget)
+            if (camera.LookAtTarget)
                 camera.Camera.transform.LookAt(target);
         }
-
-        StartCoroutine(TakeScreenshot());
     }
 
-    private IEnumerator TakeScreenshot()
+    private IEnumerator MainCoroutine()
     {
         foreach (var gameObject in pool)
         {
@@ -59,7 +112,8 @@ public class UltimateScreenshotMaker : MonoBehaviour
 
                 foreach (var camera in cameras)
                 {
-                    camera.Camera.gameObject.SetActive(true);
+                    if(cameras.Length > 1)
+                        camera.Camera.gameObject.SetActive(true);
 
                     var fileName = $"{gameObject.name}_{light.name}_{camera.Camera.name}.png";
                     ScreenCapture.CaptureScreenshot(fileName);
@@ -67,7 +121,8 @@ public class UltimateScreenshotMaker : MonoBehaviour
 
                     yield return new WaitForSeconds(0.5f);
 
-                    camera.Camera.gameObject.SetActive(false);
+                    if (cameras.Length > 1)
+                        camera.Camera.gameObject.SetActive(false);
                 }
 
                 light.enabled = false;
@@ -75,5 +130,4 @@ public class UltimateScreenshotMaker : MonoBehaviour
             gameObject.SetActive(false);
         }
     }
-
 }
